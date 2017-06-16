50a849785d04964973a3f399a384efa2cc9f5b99
Vu Le <thenewvu@gmail.com>
2017-06-16 16:13:50 +0700

    Refactored

--- browser.js
+++ browser.js
@@ -1,16 +1,7 @@
-let webview = null
-let committed = false
+const webview = document.querySelector('#webview')
 
-window.addEventListener('load', () => {
-  webview = document.querySelector('#webview')
-
-  webview.addEventListener('loadcommit', () => {
-    committed = true
-  })
-  webview.addEventListener('loadstop', () => {
-    if (committed) {
-      webview.insertCSS({ file: 'webview.css' })
-      committed = false
-    }
+webview.addEventListener('loadstop', () => {
+  webview.insertCSS({
+    file: 'webview.css'
   })
 })
